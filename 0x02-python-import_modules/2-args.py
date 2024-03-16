bac3e16a7c411eacb270c987368b9a656a5144d7
Victarr75 <korirvictor75@gmail.com>
2024-03-16 13:21:42 +0300

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,18 @@
 #!/bin/bash/python3
- import sys
+import sys
+
+
 def print_arguments():
     num_args = len(sys.argv) - 1
-    print(f"Number of argument{'s' if num_args != 1 else ''}: {num_args}", end='')
+    print(f"Number of argument{'s' if num_args !=
+          1 else ''}: {num_args}", end='')
     if num_args == 0:
         print(".")
     else:
         print(":")
         for i, arg in enumerate(sys.argv[1:], start=1):
             print(f"{i}: {arg}")
+
 
 if __name__ == "__main__":
     print_arguments()
