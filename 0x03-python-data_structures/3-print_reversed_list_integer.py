3c1e71556933e357c53c8f35031b4df7228a1606
Victarr75 <korirvictor75@gmail.com>
2024-03-19 09:28:32 +0300

    reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    # Check if the list is empty
+    if not my_list:
+        pass
+    
     # Reverse the list in place
     my_list.reverse()
     
