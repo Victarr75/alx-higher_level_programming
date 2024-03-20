989e34baa362143336769a52055f23c5e9140f6b
Victarr75 <korirvictor75@gmail.com>
2024-03-20 11:08:38 +0300

    new function

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-
-import sys
-
 if __name__ == "__main__":
-    args_count = len(sys.argv) - 1
-    if args_count == 0:
-        print("0 argument{}.".format('.' if args_count == 0 else ''))
-    else:
-        print("{} argument{}:".format(args_count, 's' if args_count > 1 else ''))
-        for i in range(1, len(sys.argv)):
-            print("{}: {}".format(i, sys.argv[i]))
+
+    from sys import argv
+    count = 1
+
+    # excludes name of program
+    number_of_args = len(argv) - 1
+    if number_of_args == 0:
+        print(f"{number_of_args:d} arguments.")
+    if number_of_args == 1:
+        print(f"{number_of_args:d} argument:")
+    if number_of_args > 1:
+        print(f"{number_of_args:d} arguments:")
+    while count < len(argv):
+        print(f"{count:d}: {argv[count]}")
+        count += 1
