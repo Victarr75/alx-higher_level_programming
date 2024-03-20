4abfff9130e5d85a3c015128038a3a7f69f6f557
Victarr75 <korirvictor75@gmail.com>
2024-03-20 10:56:42 +0300

    new function

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,11 @@
 
 import sys
 
-if len(sys.argv) == 1:
-    print("0 argument.")
-else:
-    print("{} argument{}:".format(len(sys.argv) - 1, 's' if len(sys.argv) > 2 else ''))
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("0 argument{}.".format('.' if len(sys.argv) == 1 else ''))
+    else:
+        print("{} argument{}:".format(len(sys.argv) - 1, 's' if len(sys.argv) > 2 else ''))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
 
