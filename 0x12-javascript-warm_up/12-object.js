393617ec792665e81973447d0e3ade4d5b242ad3
Victarr75 <korirvictor75@gmail.com>
2024-06-18 05:59:28 +0300

    w

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
+
 const myObject = {
   type: 'object',
   value: 12
 };
+
 console.log(myObject);
 
-myObject.value = 89;  // Updating value from 12 to 89
+myObject.value = 89; // Updating value from 12 to 89
 
 console.log(myObject);
