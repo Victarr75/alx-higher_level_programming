3ae3a91d308552be14f639f336a0e189e49358c7
Victarr75 <korirvictor75@gmail.com>
2024-07-31 01:07:00 +0300

    javascript

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,12 +4,12 @@ const fs = require('fs');
 
 // The first argument is the file path
 const file = process.argv[2];
+// The second argument is the string to write
+const text = process.argv[3];
 // The content of the file must be written in utf-8
-fs.readFile(file, 'utf8', function (err, data) {
-  // If an error occurred during the reading, print the error object
+fs.writeFile(file, text, 'utf8', function (err, data) {
+  // If an error occurred during while writing, print the error object
   if (err) {
     console.log(err);
-  } else {
-    process.stdout.write(data);
   }
 });
