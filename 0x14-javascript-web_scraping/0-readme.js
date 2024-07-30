ac83b18c26f22cf9588ba079b99c89f54eb815d1
Victarr75 <korirvictor75@gmail.com>
2024-07-31 00:53:05 +0300

    javascript

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,17 +1,10 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-const filePath = process.argv[2];
 
-if (!filePath) {
-  console.error('Usage: ./0-readme.js <file_path>');
-  process.exit(1);
-}
-
-fs.readFile(filePath, 'utf8', (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
-    console.error(err);
+    console.log(err);
   } else {
-    console.log(data);
+    console.log(data.toString());
   }
 });
